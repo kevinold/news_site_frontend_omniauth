08e1105d0ba03090ff2b9ca30ff5cdd19083f55b
Kevin Old <kevin@kevinold.com>
2014-05-03 11:05:58 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewsSiteFrontendOmniauth::Application.config.secret_key_base = 'fb4cd7f2fee3ac163733c02c60834bc7d53c4c35b1fb186be9fa02d148cc18fb744ee0924b96d35c805df021ab5643dcde1d525aa3c19215685f3730fb5f0493'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NewsSiteFrontendOmniauth::Application.config.session_store :cookie_store, key: '_news_site_frontend_omniauth_session'
