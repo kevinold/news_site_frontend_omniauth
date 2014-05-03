00f180276a5e661a97907262e082955cbb6ff704
Kevin Old <kevin@kevinold.com>
2014-05-03 11:18:22 -0500

    add twitter auth config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET']
+  #provider :twitter, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET']
+  provider :twitter, 'Yazg8rJaUO0CGb2NQxiiTdV8F', 'mFo4uSuQkykNcATJWgWFOevfkqBLSuAWqlCr3riM00xGgSDb3a'
 end
