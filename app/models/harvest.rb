1ce8657ac305ffa23895e0fa7084f0e5da692502
Aaron Tribou <aaron@triconn.net>
2014-05-03 14:02:50 -0500

    Add model data

--- app/models/harvest.rb
+++ app/models/harvest.rb
@@ -2,5 +2,10 @@ class Harvest
   include Mongoid::Document
   store_in collection: "ptwobrussell-harvest"
   field :title
-  field :source_type
+  #field :source_type
+  field :favorite_count
+  field :tweet_created_at
+  field :url
+  field :summary
+  field :source_image_url
 end
