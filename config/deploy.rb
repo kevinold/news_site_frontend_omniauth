e233280de181c28ddc328aa164e7c5d359f0b54e
Kevin Old <kevin@kevinold.com>
2014-05-03 19:37:05 -0500

    comment out reload nginx

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
-      run "sudo service nginx reload"
+      # run "sudo service nginx reload"
     end
   end
 
