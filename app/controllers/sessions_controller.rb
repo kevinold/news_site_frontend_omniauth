293072024d4479d3068e0ae7c7b05ea20097c0c4
Kevin Old <kevin@kevinold.com>
2014-05-04 17:38:48 -0500

    make headline link LIVE!!!

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
     #tweet_count = Harvest.where(:uid = user.id)
 
     #if tweet_count
-      Harvest.where(uid: user.uid).delete
+      #Harvest.where(uid: user.uid).delete
     #else
       system "python /home/ubuntu/nuztap-harvest/harvest.py #{user.uid} #{auth['credentials']['token']} #{auth['credentials']['secret']}"
     #end
