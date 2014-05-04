162479c53667dd32099f65e4e25511f2722db366
Aaron Tribou <aaron@triconn.net>
2014-05-03 23:40:49 -0500

    Format newsfeed date Add source hostname to harvest model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
 module ApplicationHelper
 end
+
+def format_newsfeed_date(dirty_date_string)
+
+  dirty_date = Time.strptime(dirty_date_string, "%a %b %d %H:%M:%S %z %Y")
+  time_now = Time.zone.now
+  difference = (time_now - dirty_date.utc).round / 60
+
+  #if within last minute
+  if dirty_date.utc > (time_now - 60)
+
+    "1 minute"
+
+  #if within last hour
+  elsif dirty_date.utc > (time_now - 60*60)
+
+    "#{difference} minutes"
+
+  #if within last day
+  elsif dirty_date.utc > (time_now - 24*60*60)
+
+    hours = difference / 60
+    "#{hours} hours"
+
+  else
+
+    dirty_date.strftime("%b %e, %Y")
+
+  end
+end
